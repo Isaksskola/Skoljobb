5d9a4a09da049d8b401e3557933a94fba5ec4806
Isaksskola <isa0908@student.skelleftea.se>
2023-12-01 13:48:46 +0100

    version 1.0

---
+++ For Loopar/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+int a;
+for(a = 200; a <20000; a = a - 100;)
+    {
+    Console.Beep(a, 500);
+}
+*/
+
+using System;
+
+
+    try
+    {
+    Console.WriteLine("Ange önskad frekvens (noll 0 avslutar): ");
+    int frekvens = int.Parse(Console.ReadLine());
+    while (frekvens != 0)
+        {
+            Console.Beep(frekvens, 2000);
+            Console.WriteLine("Ange önskad frekvens (noll 0 avslutar): ");
+            frekvens = int.Parse(Console.ReadLine());
+        }
+    }
+    catch {
+        Console.WriteLine("Felaktig frekvens. Försök igen. Frekvens skall ligga mellan 37 och 32767");
+        Console.WriteLine("Ange önskad frekvens (noll 0 avslutar): ");
+        int frekvens = int.Parse(Console.ReadLine());
+    }
+
+
+
+
